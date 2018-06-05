7fb1bef47b59c269fee62fc6512c32765ba45f2d
SG-O <sg-o@live.de>
2018-06-05 23:41:22 +0200

    Added javadoc to the VacuumDoNotDisturb class.

--- src/main/java/device/vacuum/VacuumDoNotDisturb.java
+++ src/main/java/device/vacuum/VacuumDoNotDisturb.java
@@ -11,6 +11,12 @@ public class VacuumDoNotDisturb {
     private LocalTime end;
     private boolean enabled;
 
+    /**
+     * Create a new do not disturb settings object.
+     * @param start The start time of the do not disturb period.
+     * @param end The end time of the the do not disturb period.
+     * @param enabled Weather the timer should be enabled.
+     */
     public VacuumDoNotDisturb(LocalTime start, LocalTime end, boolean enabled) {
         if (start == null) start = LocalTime.of(22, 0);
         this.start = start;
@@ -19,6 +25,11 @@ public class VacuumDoNotDisturb {
         this.enabled = enabled;
     }
 
+    /**
+     * Create a new enabled do not disturb settings object.
+     * @param start The start time of the do not disturb period.
+     * @param end The end time of the the do not disturb period.
+     */
     public VacuumDoNotDisturb(LocalTime start, LocalTime end) {
         if (start == null) start = LocalTime.of(22, 0);
         this.start = start;
@@ -27,6 +38,10 @@ public class VacuumDoNotDisturb {
         this.enabled = true;
     }
 
+    /**
+     * Generate a new enabled do not disturb settings object from the response of a device.
+     * @param dnd The response to parse.
+     */
     public VacuumDoNotDisturb(JSONArray dnd){
         JSONObject obj = dnd.optJSONObject(0);
         if (obj != null){
@@ -40,26 +55,47 @@ public class VacuumDoNotDisturb {
         }
     }
 
+    /**
+     * @return The start time of the do not disturb period.
+     */
     public LocalTime getStart() {
         return start;
     }
 
+    /**
+     * @return The end time of the do not disturb period.
+     */
     public LocalTime getEnd() {
         return end;
     }
 
+    /**
+     * @return Weather the do not disturb period is enabled.
+     */
     public boolean isEnabled() {
         return enabled;
     }
 
+    /**
+     * @param enabled True to enable, false to disable the timer.
+     */
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
 
+    /**
+     * Construct the message the controlling device sends to the vacuum.
+     * @return The constructed message.
+     */
     public JSONArray construct() {
         return construct(false);
     }
 
+    /**
+     * Construct the message the vacuum sends to the controlling device or the device sends to the vacuum.
+     * @param server True if the constructed message is in the format from vacuum to controlling device.
+     * @return The constructed message.
+     */
     public JSONArray construct(boolean server) {
         JSONArray dnd = new JSONArray();
         if (server){
