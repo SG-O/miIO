9515c25f3faef72b9d2f80599431fadf679d2174
SG-O <sg-o@live.de>
2018-06-05 23:24:55 +0200

    Added javadoc to the VacuumConsumableStatus class.

--- src/main/java/device/vacuum/VacuumConsumableStatus.java
+++ src/main/java/device/vacuum/VacuumConsumableStatus.java
@@ -34,6 +34,13 @@ public class VacuumConsumableStatus {
     private int sideBrushWorkTime = 0;
     private int filterWorkTime = 0;
 
+    /**
+     * Create a new consumable status object.
+     * @param mainBrushWorkTime The time the main brush has been running.
+     * @param sensorTimeSinceCleaning The time since the cliff sensors have been cleaned.
+     * @param sideBrushWorkTime The time the side brush has been running.
+     * @param filterWorkTime The time the filter has been in use.
+     */
     public VacuumConsumableStatus(int mainBrushWorkTime, int sensorTimeSinceCleaning, int sideBrushWorkTime, int filterWorkTime) {
         this.mainBrushWorkTime = mainBrushWorkTime;
         this.sensorTimeSinceCleaning = sensorTimeSinceCleaning;
@@ -41,6 +48,10 @@ public class VacuumConsumableStatus {
         this.filterWorkTime = filterWorkTime;
     }
 
+    /**
+     * Generate a new consumable status object from the response of a device.
+     * @param consumables The response to parse.
+     */
     public VacuumConsumableStatus(JSONObject consumables) {
         if (consumables != null) {
             this.mainBrushWorkTime = consumables.optInt(Names.MAIN_BRUSH.toString());
@@ -50,6 +61,10 @@ public class VacuumConsumableStatus {
         }
     }
 
+    /**
+     * Construct the message the vacuum sends to the controlling device.
+     * @return The constructed message.
+     */
     public JSONObject construct(){
         JSONObject payload = new JSONObject();
         payload.put(Names.MAIN_BRUSH.toString(), mainBrushWorkTime);
@@ -59,6 +74,10 @@ public class VacuumConsumableStatus {
         return payload;
     }
 
+    /**
+     * Reset a consumable.
+     * @param name The consumable to reset.
+     */
     public void reset(String name){
         if (name.equals(Names.MAIN_BRUSH.toString())) mainBrushWorkTime = 0;
         if (name.equals(Names.SENSOR.toString())) sensorTimeSinceCleaning = 0;
@@ -66,66 +85,114 @@ public class VacuumConsumableStatus {
         if (name.equals(Names.FILTER.toString())) filterWorkTime = 0;
     }
 
+    /**
+     * @return The time the main brush has been working for.
+     */
     public int getMainBrushWorkTime() {
         return mainBrushWorkTime;
     }
 
+    /**
+     * @return The time since the last time the cliff sensors have been cleaned.
+     */
     public int getSensorTimeSinceCleaning() {
         return sensorTimeSinceCleaning;
     }
 
+    /**
+     * @return The time the side brush has been working for.
+     */
     public int getSideBrushWorkTime() {
         return sideBrushWorkTime;
     }
 
+    /**
+     * @return The time the filter has been in use.
+     */
     public int getFilterWorkTime() {
         return filterWorkTime;
     }
 
+    /**
+     * @param mainBrushWorkTime The time the main brush has been working for.
+     */
     public void setMainBrushWorkTime(int mainBrushWorkTime) {
         this.mainBrushWorkTime = mainBrushWorkTime;
     }
 
+    /**
+     * @param sensorTimeSinceCleaning The time since the last time the cliff sensors have been cleaned.
+     */
     public void setSensorTimeSinceCleaning(int sensorTimeSinceCleaning) {
         this.sensorTimeSinceCleaning = sensorTimeSinceCleaning;
     }
 
+    /**
+     * @param sideBrushWorkTime The time the side brush has been working for.
+     */
     public void setSideBrushWorkTime(int sideBrushWorkTime) {
         this.sideBrushWorkTime = sideBrushWorkTime;
     }
 
+    /**
+     * @param filterWorkTime The time the filter has been in use.
+     */
     public void setFilterWorkTime(int filterWorkTime) {
         this.filterWorkTime = filterWorkTime;
     }
 
+    /**
+     * @return The time until the main brush should be changed.
+     */
     public int getMainBrushWorkTimeLeft() {
         return MAIN_BRUSH_TIME_BETWEEN_CHANGE - mainBrushWorkTime;
     }
 
+    /**
+     * @return The time until the cliff sensors should be cleaned.
+     */
     public int getSensorTimeSinceCleaningLeft() {
         return SENSOR_TIME_BETWEEN_CLEANING - sensorTimeSinceCleaning;
     }
 
+    /**
+     * @return The time until the side brush should be changed.
+     */
     public int getSideBrushWorkTimeLeft() {
         return SIDE_BRUSH_TIME_BETWEEN_CHANGE - sideBrushWorkTime;
     }
 
+    /**
+     * @return The time until the filter should be changed.
+     */
     public int getFilterWorkTimeLeft() {
         return FILTER_TIME_BETWEEN_CHANGE - filterWorkTime;
     }
 
+    /**
+     * @return The approximate usage of the main brush.
+     */
     public float getMainBrushWorkPercent() {
         return ((float)mainBrushWorkTime) / ((float) MAIN_BRUSH_TIME_BETWEEN_CHANGE) * 100.0f;
     }
 
+    /**
+     * @return The approximate dirtiness of the cliff sensor.
+     */
     public float getSensorPercentSinceCleaning() {
         return ((float)sensorTimeSinceCleaning) / ((float) SENSOR_TIME_BETWEEN_CLEANING) * 100.0f;
     }
 
+    /**
+     * @return The approximate usage of the side brush.
+     */
     public float getSideBrushWorkPercent() {
         return ((float)sideBrushWorkTime) / ((float) SIDE_BRUSH_TIME_BETWEEN_CHANGE) * 100.0f;
     }
 
+    /**
+     * @return The approximate dirtiness of the filter.
+     */
     public float getFilterWorkPercent() {
         return ((float)filterWorkTime) / ((float) FILTER_TIME_BETWEEN_CHANGE) * 100.0f;
     }
